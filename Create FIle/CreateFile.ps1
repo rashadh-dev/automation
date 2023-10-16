2469dbf15b38cb9033fac5981775e01a9bd282d0
RASHAD H <141707869+rashadh-dev@users.noreply.github.com>
2023-10-16 14:43:44 +0530

    Add files via upload
    Monthy creation of Timesheet.xlsx added

--- Create FIle/CreateFile.ps1
+++ Create FIle/CreateFile.ps1
@@ -39,7 +39,7 @@ if (-Not (Test-Path -Path $textFileName)) {
     Write-Host "File already exists: $textFileName"
 }
 
-# You can add content to the text file if needed
+# how to add content to the text file if needed
 # Add-Content -Path $textFileName -Value "This is the content of the file."
 
 $pathToOpen = "$baseDir$year\$month\$week\$date-$shortDay ($myday).txt"
@@ -52,3 +52,61 @@ $pathToOpen = "$baseDir$year\$month\$week\$date-$shortDay ($myday).txt"
 #     Write-Host "Visual Studio Code is not installed. Please install it to open the file."
 # }
 # PAUSE
+
+
+
+
+##
+## THE FOLLOWING PROGRAM CHECKS AND CREATE A TIMESHEET IF NO EXISTS
+##
+
+$currentDate = Get-Date
+$year = $currentDate.Year
+$month = $currentDate.Month
+$day = (Get-Date -Year $year -Month $month -Day 1).AddMonths(1).AddDays(-1).Day
+$currentMonth = $currentDate.ToString("MMMM")
+
+$sourceFilePath = "C:\Users\hrashad\OneDrive - newdreamdatasystems.com\Note\Timesheet_source.xlsx"
+$destinationFilePath = "C:\Users\hrashad\OneDrive - newdreamdatasystems.com\Note\$year\$currentMonth\Timesheet - $currentMonth $year.xlsx"
+
+
+if (-Not (Test-Path -Path $destinationFilePath)) {
+
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $false
+$sourceWorkbook = $excel.Workbooks.Open($sourceFilePath)
+$sourceSheet = $sourceWorkbook.Sheets.Item("source")
+
+while ($day -ge 1) {
+    $date = Get-Date -Year $year -Month $month -Day $day
+
+    if ($date.DayOfWeek -ne [System.DayOfWeek]::Saturday -and $date.DayOfWeek -ne [System.DayOfWeek]::Sunday) {
+        $sheetName = $date.ToString("dd MMM")
+        Write-Host $sheetName
+
+        $newSheet = $sourceWorkbook.Sheets.Item(1)
+        $sourceSheet.Copy($newSheet)
+        $newSheet.Name = $sheetName
+
+        $sheetDate = $date.ToString("MM/dd/yyyy")
+        $newSheet.Cells.Item(8, 1) = $sheetDate
+    }
+    $day--
+}
+
+$sourceWorkbook.SaveAs($destinationFilePath)
+$sourceWorkbook.Close($false)
+$excel.Quit()
+
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($newSheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($sourceSheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($sourceWorkbook) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
+
+Stop-Process -Name Excel -Force
+
+Write-Host "Saved to '$destinationFilePath'."
+
+}else {
+    Write-Host "Timesheet already exists: $destinationFilePath"
+}
